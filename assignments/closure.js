4c7794c311e6048fbd7285daa5d3cf553edab8a8
Orie Chinedu <oriechinedu@gmail.com>
2019-05-07 13:27:03 +0100

    (stretch closure): Implement Counter

--- assignments/closure.js
+++ assignments/closure.js
@@ -27,14 +27,46 @@ X() // returns 'The outer array length is equal to the inner array length'
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return () => {
+    count += 1;
+    console.log(count);
+  }
 };
+
+
 // Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+const newCounter = counter();
+newCounter(); // 1
+newCounter(); // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let count = 0;
+  return {
+    increment: function() {
+      ++count;
+      console.log(count)
+    },
+    decrement: function() {
+      --count;
+      console.log(count)
+    }
+  }
 };
+
+const newCounterFactory = counterFactory()
+
+newCounterFactory.increment() // 1
+newCounterFactory.increment() // 2
+newCounterFactory.increment() // 3
+newCounterFactory.increment() //4
+
+newCounterFactory.decrement() //3
+newCounterFactory.decrement() // 2
+newCounterFactory.decrement() // 1
+
+
